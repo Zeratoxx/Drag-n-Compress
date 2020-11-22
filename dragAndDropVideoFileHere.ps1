f6fc6f25bcc92f327d9b3b48a8d636dbb4dfbac0
Nils Badtke <nils.badtke@stud.h-da.de>
2020-11-22 01:31:35 +0100

    fix finished and working

--- dragAndDropVideoFileHere.ps1
+++ dragAndDropVideoFileHere.ps1
@@ -1,5 +1,40 @@
-# Compress Drag&Dropped video files.
+# --------- Script section ---------
 
+$script = {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Position=0)]
+        [string]$Arg1,
+    
+        [Parameter(Position=1)]
+        [string]$Arg2
+    )
+    
+    Write-Host -ForegroundColor Yellow "inputFile: $Arg1"
+    Write-Host -ForegroundColor Yellow "outputFile: $Arg2"
+    Write-Host ""
+
+    .\ffmpeg.exe -i "$Arg1" -vcodec libx264 -crf 24 "$Arg2"
+    Write-Host ""
+    Write-Host -ForegroundColor Green "Compression finished.`noutputFile: $Arg2"
+    PAUSE;
+}
+
+
+# --------- Function section ---------
+
+function Run-InNewProcess {
+    param([String] $code)
+    $code = "function Run{ $code }; Run $args"
+    $encoded = [Convert]::ToBase64String( [Text.Encoding]::Unicode.GetBytes($code))
+    
+    start-process PowerShell.exe -argumentlist '-encodedCommand',$encoded
+}
+
+
+# ----------- Code section -----------
+
+# Compress Drag&Dropped video files.
 FOREACH ($inputFile in $args) {
     $splittedFileName = $inputFile.Split("{.}")
     $outputFile = ""
@@ -27,7 +62,7 @@ Starting compression in an own process...
 
 "@
         cd $PSScriptRoot
-        START powershell {cd $PSScriptRoot; .\ffmpeg.exe -i $inputFile -vcodec libx264 -crf 24 $outputFile; PAUSE}
+        Run-InNewProcess $script -Arg1 "`"$inputFile`"" -Arg2 "`"$outputFile`""
 
     } ELSE {
         Write-Host -ForegroundColor Red @"
